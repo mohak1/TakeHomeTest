219c74702f656c960e864fef8c8d6e571b8ed8f2
mohak <narangmohak9@gmail.com>
2023-05-13 14:29:21 +0100

    adding tests for tasks.py

---
+++ app/tests/test_tasks.py
@@ -0,0 +1,16 @@
+"""This file contains unit tests for functions in `tasks.py`"""
+
+import unittest
+
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+class TestTasks(unittest.TestCase):
+    def test_perform_task_1(self):
+        self.assertEqual(True, False)
+
+    def test_perform_task_2(self):
+        self.assertEqual(True, False)
+
+    def test_perform_task_3(self):
+        self.assertEqual(True, False)
