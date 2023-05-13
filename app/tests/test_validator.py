6d40de48273ebc7e5618969a859feda344e65267
mohak <narangmohak9@gmail.com>
2023-05-13 14:22:37 +0100

    adding test file for validator.py

---
+++ app/tests/test_validator.py
@@ -0,0 +1,11 @@
+"""This file contains unit tests for functions in `validator.py`"""
+
+import unittest
+
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+
+class TestValidator(unittest.TestCase):
+
+    def test_check_command_line_args(self):
+        self.assertEqual(True, False)
