2b80c916de3b966298e4ec5a856fd5c7f5a8ac7c
mohak <narangmohak9@gmail.com>
2023-05-14 11:51:48 +0100

    updating config: adding values

--- app/config.py
+++ app/config.py
@@ -4,27 +4,38 @@ used in the code
 """
 import datetime
 
-# TODO: replace column name string values with ENUMs
+# TODO: replace column name string values with ENUMs
 
 URL = 'http://www.fifeweather.co.uk/cowdenbeath/200606.csv'
+CHUNK_SIZE = 1024
 OUTPUT_DIR = ''
 T1_FILE_NAME = ''
 T2_FILE_NAME = ''
 T3_FILE_NAME = ''
-EXPECTED_COL_NAMES = {} # column names that the CSV file should have
+
+# column names that are required in the CSV file for the tasks
+EXPECTED_COL_NAMES = [
+    'Date', 'Time', 'Outside Temperature', 'Hi Temperature',
+    'Low Temperature',
+]
+
+# columns from EXPECTED_COL_NAMES that are expected to have numeric data
+NUMERIC_COL_NAMES = [
+    'Outside Temperature', 'Hi Temperature', 'Low Temperature',
+]
 
 # constants used for Task 1
 T1_COL_NAME = 'Outside Temperature'
 
 # constants used for Task 2
-T2_START_DATE = datetime.datetime.strptime('01-06-2006', '%d/%m/%Y')
-T2_END_DATE = datetime.datetime.strptime('09-06-2006', '%d/%m/%Y')
+T2_START_DATE = datetime.datetime.strptime('01/06/2006', '%d/%m/%Y')
+T2_END_DATE = datetime.datetime.strptime('09/06/2006', '%d/%m/%Y')
 T2_COL_TEMP_RANGE = {
     'Hi Temperature': (21.3, 23.3),
     'Low Temperature': (10.1, 10.5),
 }
 
-# constants used for Task 3
+# constants used for Task 3
 T3_COL_NAME = 'Outside Temperature'
 AVERAGE_TEMP = 25
 T3_NUM_DAYS = 9
