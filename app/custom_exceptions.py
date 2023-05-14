3d18f86b148c29d779258382279f93213a54fce7
mohak <narangmohak9@gmail.com>
2023-05-14 11:54:40 +0100

    adding custom_exceptions file with 'InvalidFormatError'

---
+++ app/custom_exceptions.py
@@ -0,0 +1,7 @@
+"""Contains custom exceptions that are used in the script"""
+
+# pylint: disable=unnecessary-pass
+
+class InvalidFormatError(Exception):
+    """Exception raised when an object is not in the expected format"""
+    pass
