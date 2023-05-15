1abba630e7a1926c8b2c52f83574bb67d27ef89d
mohak <narangmohak9@gmail.com>
2023-05-15 03:00:39 +0100

    config.py: updating values for task2

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ URL = 'http://www.fifeweather.co.uk/cowdenbeath/200606.csv'
 CHUNK_SIZE = 1024
 OUTPUT_DIR = './output'
 T1_FILE_NAME = 'task1.txt'
-T2_FILE_NAME = ''
+T2_FILE_NAME = 'task2.txt'
 T3_FILE_NAME = ''
 
 # column names that are required in the CSV file for the tasks
@@ -31,10 +31,11 @@ T1_COUNT_OF_TOP_HOTTEST_DAYS = 10
 # constants used for Task 2
 T2_START_DATE = datetime.datetime.strptime('01/06/2006', '%d/%m/%Y')
 T2_END_DATE = datetime.datetime.strptime('09/06/2006', '%d/%m/%Y')
-T2_COL_TEMP_RANGE = {
-    'Hi Temperature': (21.3, 23.3),
-    'Low Temperature': (10.1, 10.5),
-}
+T2_COL_TEMP_RANGE = [
+    # column name, range start value, range end value (both inclusive)
+    ('Hi Temperature', 21.3, 23.3),
+    ('Low Temperature', 10.1, 10.5),
+]
 
 # constants used for Task 3
 T3_COL_NAME = 'Outside Temperature'
